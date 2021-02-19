14c09f6f75e4e2c1cdd4d05368df8e775400f996
hkkku <hhk7045@gmail.com>
2021-02-19 10:11:21 +0900

    02/19 웹사이트 리뉴얼 메인 배너 & 네비게이션 디자인

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>리슬</title>
+
+  <!--  Awesome Font link -->
+  <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+  />
+  <!-- Reset CSS Link -->
+  <link rel="stylesheet" href="/leesle/css/reset.css">
+  <!-- Main CSS Link -->
+  <link rel="stylesheet" href="/leesle/css/style.css">
+  <link rel="stylesheet" href="/leesle/css/media.css">
+
+</head>
+<body>
+
+  <!-- All contents are in WRAPS -->
+  <div class="wrap">
+    <header>
+      <div class="headerWrap">
+        <div class="logo">
+          <img src="/leesle/img/logo.png" alt="">
+        </div>
+        <ul class="gnb clear">
+          <li><a href="">SHOP</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">COLLABO</a></li>
+          <li><a href="">LOOK</a></li>
+          <li><a href="">Q&A</a></li>
+        </ul>
+      </div>
+      <div class="banner">
+        <img src="/leesle/img/main_banner.jpg" alt="">
+      </div>
+    </header>
+    <div class="center">
+
+    </div>
+    <!-- center div end -->
+  </div>
+  <!-- wrap div end -->
+  
+</body>
+</html>
